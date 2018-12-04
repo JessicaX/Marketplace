9e735dfff04b9014c83692f3d2361a5af61dd2d8
Jessica Yizhang <jessica_x_1028@hotmail.com>
2018-12-03 23:54:14 -0500

    add html templates & functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, session
+from flask import Flask, render_template, request, redirect, jsonify, session, flash
 from sql_client import MySqlClient
 import utils
 import cloudinary
@@ -8,11 +8,15 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 client = MySqlClient()
+@app.template_filter('resize')
+def resize_filter(s, size):
+    return s.replace("image/upload", "image/upload/"+size)
 
 @app.route("/")
 def index():
     items = client.get_latest_items()
-    return render_template('index.html', items=items)
+    categories = client.get_all_categories()
+    return render_template('index.html', items=items, categories=categories)
 
 @app.route("/checkusername/<username>")
 def check_username_usability(username):
@@ -37,8 +41,10 @@ def login():
     username = data["username"].strip()
     password = data["password"]
     user = client.get_user_by_username_password(username, utils.md5_hash(password))
+    # join_date = client.get_user_by_join_date(username)
     if user is not None:
         session["user"] = user
+        # session["join_date"] = join_date
     else:
         flash(u'Login failed. Wrong username or password.', 'danger')
     return redirect("/")
@@ -50,20 +56,136 @@ def logout():
 
 @app.route("/sell")
 def sell():
-    return render_template("sell.html")
+    categories = client.get_all_categories()
+    return render_template("sell.html", categories=categories)
 
-@app.route("/singlepage")
-def signlepage():
-    return render_template("singlepage.html")
+@app.route("/createitem", methods=["POST"])
+def create_item():
+    data = request.form
+    user = session["user"]
+    item = client.create_item(user["Id"], data)
+    categories = data.getlist('Category')
+    print(categories)
+    if len(categories) > 0:
+        client.add_categories_to_item(item[0]["Id"], categories)
+    return redirect("/edititem/%s"%item[0]["Id"])
 
-@app.route("/uploadimage", methods=["POST"])
-def upload():
+@app.route("/edititem/<item_id>")
+def edit_item(item_id):
+    print(request.referrer)
+    item = client.get_item_by_id(item_id)[0]
+    categories = [d["categoryid"] for d in client.get_item_categories(item_id)]
+    images = client.get_images_by_item_id(item_id)
+    allcategories = client.get_all_categories()
+    return render_template("edit.html", item=item, images=images, categories=categories, allcategories=allcategories)
+
+@app.route("/updateitem", methods=["POST"])
+def update_item():
+    try:
+        data = request.form
+        id = data["Id"]
+        categories = data.getlist("Category")
+        client.update_item(data)
+        o_categories = [str(c["categoryid"]) for c in client.get_item_categories(id)]
+        print(o_categories)
+        i_categories = []
+        d_categories = []
+        for c in categories:
+            if c not in o_categories:
+                i_categories.append(c)
+
+        for o in o_categories:
+            if o not in categories:
+                d_categories.append(o)
+
+        print(i_categories)
+        print(d_categories)
+
+        if len(i_categories) > 0:
+            client.add_categories_to_item(id, i_categories)
+
+        if len(d_categories) > 0:
+            client.delete_categories(id, d_categories)
+
+        flash(u'Update successfully.', 'success')
+        return redirect("/edititem/"+id)
+    except Exception as ex:
+        print(str(ex))
+        flash(u'Update failed!', 'danger')
+
+@app.route("/singlepage/<id>")
+def signlepage(id):
+    item = client.get_item_by_id(id)[0]
+    images = client.get_images_by_item_id(id)
+    likes = client.get_item_likes(session['user']['Id'], id)
+    return render_template("singlepage.html", item=item, images=images, likes=likes[0])
+
+@app.route("/profilepage")
+def profilepage():
+    items = client.get_latest_items()
+    return render_template('profilepage.html', items=items)
+
+@app.route("/wishing")
+def wishing():
+    items = client.get_latest_items()
+    return render_template('wishing.html', items=items)
+
+@app.route("/category/<category_id>")
+def category(category_id):
+    items = client.get_item_by_category_id(category_id)
+    # items = client.get_item_by_category_id(session["category"]["Id"])
+    return render_template('category.html', items=items)
+
+@app.route("/uploadimage/<itemid>", methods=["POST"])
+def upload(itemid):
     cloudinary.config(
         cloud_name='yybird', 
-        api_key= 'xx', # please check with me the api_key 
-        api_secret= 'xx' # please check with me the parameter
+        api_key= 'xxx',
+        api_secret= 'xxx'
     )
     files = request.files.getlist("files[]")
     for file in files:
-        print(cloudinary.uploader.upload(file))
-    return redirect("/sell")
+        resp = cloudinary.uploader.upload(file)
+        print(resp)
+        client.add_image_to_item(itemid, resp["url"])
+    return redirect("/edititem/%s"%itemid)
+
+@app.route("/deleteimage/<item_id>", methods=["POST"])
+def deleteimg(item_id):
+    data = request.form#{"imageid": 1}
+    id = data["imageid"]
+    client.delete_image_by_imageid(id)
+    flash(u'Delete successfully.', 'success')
+    return redirect("/edititem/"+item_id)
+
+@app.route("/like/<item_id>", methods=["POST"])
+def like_item(item_id):
+    client.like_item(session['user']["Id"], item_id)
+    return redirect("/singlepage/%s"%item_id)
+
+@app.route("/comment/<item_id>", methods=["POST"])
+def make_comment(item_id):
+    message = request.form["message"]
+    client.make_comment(session['user']['Id'], item_id, message)
+    return redirect("/singlepage/%s"%item_id)
+
+@app.route("/myitems")
+def my_items():
+    items = client.get_my_items(session["user"]["Id"])
+    return render_template("myitem.html", items=items)
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/myfavourites")
+def my_favourites():
+    items = client.get_my_favourites(session["user"]["Id"])
+    return render_template("myfavourites.html", items=items)
+
+@app.route("/searchpage")
+def searchpage():
+    searchitem = request.args.get("searchitem")
+    items = client.get_search_items_by_name(searchitem)
+    # categories = client.get_all_categories()
+    return render_template('searchpage.html', items=items, searchitem=searchitem)
--- sql_client.py
+++ sql_client.py
@@ -10,11 +10,21 @@ class MySqlClient():
             database="cs6400"
         )
 
+    def get_all_categories(self):
+        return self._select("select * from categories")
+
     def get_latest_items(self):
         mycursor = self.mydb.cursor(buffered=True)
-        mycursor.execute('''SELECT u.Name as Username, i.* FROM items i 
-                            INNER JOIN users u ON i.UserId = u.Id
-                            LIMIT 20;''')
+        mycursor.execute("""select it.`*`, y.imageurl
+                            from items it
+                                inner join users u on it.UserId = u.id
+                                left join (
+                                    select itemid, min(id) as min_id
+                                    from images i
+                                    group by itemid
+                                ) x on it.Id = x.itemid
+                                left join images y on y.id = x.min_id
+                            limit 20;""")
         result = []
         columns = tuple( [d[0] for d in mycursor.description] )
         for row in mycursor:
@@ -50,14 +60,153 @@ class MySqlClient():
         else:
             return None
 
+    def add_image_to_item(self, item, link):
+        cursor = self.mydb.cursor()
+        cursor.execute("insert into images (itemid, imageurl) values (%s, '%s');"%(item, link))
+        self.mydb.commit()
+
+    def get_images_by_item_id(self, item_id):
+        return self._select("select * from images where itemid = %s order by id"%item_id)
+
+    def create_item(self, userid, data):
+        cursor = self.mydb.cursor()
+        query = """insert into items (UserId, Name, Description, Price, Likes, Location, TradeMethod, Status, CreatedOn)
+                          values(%s, '%s', '%s', %s, %s, '%s', '%s', %s, '%s')"""%(userid, data["Name"], data["Description"], data["Price"], 0, data["Location"], data["TradeMethod"], 0, datetime.now())
+        cursor.execute(query)
+        self.mydb.commit()
+        result = self._select("select * from items where userId = %s order by id desc limit 1;" % userid)
+        return result
+
+    def add_categories_to_item(self, item_id, categories):
+        cursor = self.mydb.cursor()
+        l = []
+        for category in categories:
+            l.append("(%s, %s)"%(item_id, category, ))
+        query = "insert into item_category (itemid, categoryid) values " + ",".join(l)
+        print(query)
+        cursor.execute(query)
+        self.mydb.commit()
+
+    def update_item(self, data):
+        cursor = self.mydb.cursor()
+        query = """update items set Name='%s', Description='%s', Price=%s,
+                    Location='%s', TradeMethod='%s'
+                    where Id = %s
+                    """%(data["Name"], data["Description"], data["Price"], data["Location"], data["TradeMethod"],data["Id"])
+        cursor.execute(query)
+        self.mydb.commit()
+        
+    def delete_categories(self, item_id, categories):
+        cursor = self.mydb.cursor()
+        query = """delete from item_category where itemid = %s and categoryid in (%s)"""%(item_id, ",".join(categories))
+        cursor.execute(query)
+        self.mydb.commit()
+
+    def get_item_by_id(self, item_id):
+        return self._select("select * from items where id = %s;" % item_id)
+
+    def get_latest_items_by_category(self, item_id):
+        return self._select("""select *
+                               from items as i INNER JOIN item_category as j
+                               ON i.id = j.itemid
+                               where j.categoryid = %s;""" % item_id)
+                               
+    def get_item_categories(self, item_id):
+        return self._select("""select categoryid
+                             from item_category
+                             where itemid = %s"""%item_id)
+
+    def get_images_by_itemid(self, itemid):
+        return self._select("select * from images where itemid = %s"%itemid)
+
+    def delete_image_by_imageid(self, imageid):
+        cursor = self.mydb.cursor()
+        cursor.execute("delete from images where id = %s"%imageid)
+        self.mydb.commit()
+
+    def like_item(self, user_id, item_id):
+        cursor = self.mydb.cursor()
+        cursor.execute("insert into item_like (itemid, userid) values (%s, %s)"%(item_id, user_id))
+        self.mydb.commit()
+
+    def get_item_likes(self, user_id, item_id):
+        cursor = self.mydb.cursor()
+        return self._select('''
+                                select count(1) as Total, count(u.id) as mycount
+                                from item_like il
+                                    left join users u on il.userid = u.id
+                                where il.itemid = %s and u.id = %s
+                            '''%(item_id, user_id))
+
+    def make_comment(self, user_id, item_id, message):
+        cursor = self.mydb.cursor()
+        cursor.execute("insert into comments (userid, itemid, content, createdon) values (%s, %s, %s, %s)"%(user_id, item_id, content, datetime.now()))
+        self.mydb.commit()
+
+    def get_my_items(self, user_id):
+        # return self._select("select * from items where userid = %s order by createdon desc"%user_id)
+        mycursor = self.mydb.cursor(buffered=True)
+        mycursor.execute("""select it.`*`, y.imageurl
+                            from items it
+                                inner join users u on it.UserId = u.id
+                                left join (
+                                    select itemid, min(id) as min_id
+                                    from images i
+                                    group by itemid
+                                ) x on it.Id = x.itemid
+                                left join images y on y.id = x.min_id
+                            where it.UserId = %s;"""%(user_id))
+        result = []
+        columns = tuple( [d[0] for d in mycursor.description] )
+        for row in mycursor:
+            result.append(dict(zip(columns, row)))
+        return result
+
+    def get_search_items_by_name(self,searchitem):
+        mycursor = self.mydb.cursor(buffered=True)
+        pattern = "%" + searchitem + "%"
+        mycursor.execute("""select it.`*`, y.imageurl
+                            from items it
+                                inner join users u on it.UserId = u.id
+                                left join (
+                                    select itemid, min(id) as min_id
+                                    from images i
+                                    group by itemid
+                                ) x on it.Id = x.itemid
+                                left join images y on y.id = x.min_id
+                                where it.name like '%s';"""%(pattern))
+                            
+        result = []
+        columns = tuple( [d[0] for d in mycursor.description] )
+        for row in mycursor:
+            result.append(dict(zip(columns, row)))
+        return result
+
+    def get_my_favourites(self, user_id):
+        return self._select("select i.* from items i inner join item_like l on i.id = l.itemid where l.userid = %s order by l.id desc"%user_id)
+
+    def get_item_by_category_id(self, category_id):
+        return self._select("""select * from items i inner join item_category ic on i.id = ic.itemid where ic.categoryid = %s order by i.id desc"""%category_id)
+        # mycursor = self.mydb.cursor(buffered=True)
+        # mycursor.execute("""select ic.`*`, y.imageurl
+        #                     from item_category ic
+        #                         inner join items i on ic.itemid = i.id
+        #                         left join (
+        #                             select itemid, min(id) as min_id
+        #                             from images i
+        #                             group by itemid
+        #                         ) x on i.Id = x.itemid
+        #                         left join images y on y.id = x.min_id
+        #                     where ic.categoryid = %s;"""%(category_id))
+        # result = []
+        # columns = tuple( [d[0] for d in mycursor.description] )
+        # for row in mycursor:
+        #     result.append(dict(zip(columns, row)))
+        # return result
+
     def _select(self, sql):
         mycursor = self.mydb.cursor(buffered=True)
         mycursor.execute(sql)
         return self._parse_result(mycursor)
 
     def _parse_result(self, mycursor):
-        result = []
-        columns = tuple( [d[0] for d in mycursor.description] )
-        for row in mycursor:
-            result.append(dict(zip(columns, row)))
-        return result
